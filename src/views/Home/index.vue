2612fdc4632b6fd2bd822c8007be2518a210aa83
xianjie <zzhjsan@gmail.com>
2021-03-30 17:54:50 +0800

    update num-roll

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -31,6 +31,14 @@
               <span>接入秤数</span>
             </li>
           </ul>
+          <h4>今日交易额</h4>
+          <ul class="num-roll">
+            <li class="no-date" v-for="index in 9" :key="index">
+              <span>
+                <i :ref="el => { numberItem[index-1] = el }">0123456789</i>
+              </span>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
@@ -40,14 +48,17 @@
 <script>
 import echarts from 'echarts'
 import shenzhen from '@/assets/map/shenzhen.json'
-import { onMounted } from 'vue'
+import { nextTick, onMounted, ref } from 'vue'
 import {mapOption } from "@/views/Home/components/map";
 
 export default {
   name: 'Home',
   setup() {
-    onMounted(() => (initMap()))
-
+    const numberArr = ref([])
+    const numberItem = ref([])
+    onMounted(() => {
+      initMap()
+    })
     const initMap = () => {
       mapOption.series[0].data = [{name:'食堂', value: [114.23102, 22.541309, 2], symbolSize: 10}]
       let chart = echarts.init(document.getElementById('map'))
@@ -56,7 +67,37 @@ export default {
       window.addEventListener('resize', () => {
         chart.resize();
       })
+      let num = 2568782
+      setNumberTransform(num)
+      setInterval(() => {
+        num ++
+        setNumberTransform(num)
+      }, 5000)
+
     }
+    const setNumberTransform = (num) => {
+      num = `${num}`.split('')
+      const numberItems = numberItem.value
+      if (num.length < 9) {
+        let arrLength = num.length
+        for (let i = 0; i < (9 - arrLength); i++) {
+          num.unshift('0')
+        }
+      }
+      nextTick(() => {
+        numberItems.forEach((item,index) => {
+          let ty = num[index]
+          if (ty === '0') {
+            item.style = 'opacity: .3;'
+          } else {
+            item.style = `transform:translate(-50%, -${ty * 10}%);`
+          }
+
+        })
+      })
+    }
+
+    return { numberItem,numberArr }
   }
 }
 </script>
