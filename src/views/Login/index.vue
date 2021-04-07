763c4053fc7607e03af45191fb9feec2bd56ddcf
xianjie <zzhjsan@gmail.com>
2021-04-07 14:26:30 +0800

    add login

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="login-box">
     <div class="login-tit">用户登录</div>
+    <div class="form">
+      <input type="text" placeholder="请输入登录ID">
+      <input type="password" placeholder="请输入密码">
+      <label>
+        <input type="text" placeholder="请输入验证码">
+        <em/>
+      </label>
+    </div>
   </div>
 </template>
